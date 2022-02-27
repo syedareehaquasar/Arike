461cd9bbba377e0b54bbbfcc5b1ef6d4d228e4b9
syedareehaquasar <reeha.saba@gmail.com>
2022-02-27 17:22:32 +0530

    this is my test file

--- management/views.py
+++ management/views.py
@@ -12,23 +12,18 @@ TYPES_OF_USERS = (
     ("district_admin", "district_admin")
 )
 
-class UserCreateForm(forms.Form):
-    full_name = forms.CharField(max_length=100, required=True)
-    email = forms.EmailInput()
-    phone = forms.CharField(max_length=14)
-    is_verified = forms.BooleanField()
-    role = forms.ChoiceField(choices=TYPES_OF_USERS)
-    password = forms.PasswordInput()
+class CustomUserForm(forms.ModelForm):
+    # full_name = forms.CharField(max_length=100)
+    # email = forms.EmailInput()
+    # phone = forms.CharField(max_length=14)
+    # is_verified = forms.BooleanField()
+    # role = forms.ChoiceField(choices=TYPES_OF_USERS)
+    # password = forms.PasswordInput()
 
     class Meta:
         model = User
-
-    def save(self, commit=True):
-        if not commit:
-            raise NotImplementedError("Can't create User and UserProfile without database save")
-        user = super(UserCreateForm, self).save(commit=True)
-        return user
+        fields = ["full_name", "email", "phone", "is_verified", "role"]
 
 class UserCreateView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserForm
     template_name = "./user/signup.html"
