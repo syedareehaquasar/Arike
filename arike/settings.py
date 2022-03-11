673df5feb30ef56c3cba71e436eeefe00184f744
syedareehaquasar <reeha.saba@gmail.com>
2022-03-12 00:38:10 +0530

    created and added more forms

--- arike/settings.py
+++ arike/settings.py
@@ -74,6 +74,13 @@ WSGI_APPLICATION = 'arike.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql_psycopg2", 
@@ -127,4 +134,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/dashboard'
 
--- management/forms.py
+++ management/forms.py
@@ -3,17 +3,37 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm, Authenti
 from .models import *
 
 class MyUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["phone"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["is_verified"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["role"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["facility"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["district"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["user"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
 
     class Meta:
         model = UserProfile
-        fields = ("phone", "is_verified", "role", "facility", "district")
+        fields = ("phone", "is_verified", "role", "facility", "district", "user")
 
 
 class MyUserChangeForm(UserChangeForm):
 
     class Meta(UserChangeForm):
         model = UserProfile
-        fields = ("phone", "is_verified", "role", "facility", "district")
+        fields = ("phone", "is_verified", "role", "facility", "district", "deleted")
 
 class CustomAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
@@ -32,16 +52,93 @@ class FacilityCreationForm(forms.ModelForm):
         fields = ("kind", "name", "address", "pincode", "phone", "ward", "deleted")
 
 class AddPatientForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["full_name"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["date_of_birth"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["address"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["landmark"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["phone"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["gender"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["emergency_phone_number"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["ward"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["facility"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+        self.fields["expired_time"].widget.attrs[
+            "class"
+        ] = "p-1 mb-2 bg-gray-200/75 rounded-lg w-full"
+
     class Meta(forms.ModelForm):
         model = Patient
-        fields = ("full_name", "date_of_birth", "address", "landmark", "phone", "gender", "emergency_phone_number", "expired_time", "ward", "facility", "deleted")
+        fields = ("full_name", "date_of_birth", "address", "landmark", "phone", "gender", "emergency_phone_number", "ward", "facility", "deleted", "expired_time")
 
-# class AddFamilyDetailsForm(UserCreationForm):
-#     class Meta(UserCreationForm):
-#         model = UserProfile
-#         fields = ("phone", "email", "is_verified", "role", "facility", "ward")
+class AddFamilyDetailsForm(forms.ModelForm):
 
-# class addPatientDisease(UserCreationForm):
-#     class Meta(UserCreationForm):
-#         model = UserProfile
-#         fields = ("phone", "email", "is_verified", "role", "facility", "ward")
+    class Meta(forms.ModelForm):
+        model = Family_Detail
+        fields = ("full_name", "date_of_birth", "phone", "email", "relation", "address", "education", "occupation", "remarks", "is_primary", "patient", "deleted")
+
+
+class addPatientDisease(forms.ModelForm):
+    class Meta(forms.ModelForm):
+        model = Patient_Disease
+        fields = ("patient", "disease", "treatment", "note", "nurse", "deleted")
+
+class addDisease(forms.ModelForm):
+    class Meta(forms.ModelForm):
+        model = Disease
+        fields = ("name", "icds_code")
+
+class addTreatment(forms.ModelForm):
+    class Meta(forms.ModelForm):
+        model = Treatment
+        fields = ("description", "care_type", "care_sub_type", "patient", "nurse", "deleted")
+
+class scheduleVisit(forms.ModelForm):
+    class Meta(forms.ModelForm):
+        model = Schedule_Visit
+        fields = ("date", "time", "duration", "patient", "nurse", "deleted")
+
+class addVisitDetails(forms.ModelForm):
+    class Meta(forms.ModelForm):
+        model = Visit_Details
+        fields = ("palliative_phase", "blood_pressure", "pulse", "general_random_blood_sugar", "personal_hygiene", "mouth_hygiene", "pubic_hygiene", "systemic_examination", "patient_at_peace", "pain", "symptoms", "note", "visit_schedule")
+
+class addWard(forms.ModelForm):
+    class Meta(forms.ModelForm):
+        model = Ward
+        fields = ("name", "number", "lsg")
+    
+class addLSG(forms.ModelForm):
+    class Meta(forms.ModelForm):
+        model = lsg_body
+        fields = ("name", "kind", "lsg_body_code", "district")
+
+
+class addDistrict(forms.ModelForm):
+    class Meta(forms.ModelForm):
+        model = District
+        fields = ("name", "state")
+
+
+class addState(forms.ModelForm):
+    class Meta(forms.ModelForm):
+        model = State
+        fields = ("name",)
